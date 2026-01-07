57ffb24119860ee0ac2779613d357e14141921da
frator99 <francescotorraco999@gmail.com>
2026-01-07 20:58:05 +0100

    implementazione 'add' button

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
-def get_todos(filename="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filename=FILEPATH):
     """Esempio di docstring , questo testo racchiuso tra 3 apici consiste nella codumentazione di una funzione.
     Per printare la docunmentazione si può fare : help(<nome_funzione>)"""
     with open(filename, 'r') as file:
-        todos = file.readlines()
+        todos_local = file.readlines()
         return todos_local
 def write_todos(todos_arg, filepath="todos.txt"): # qui mentre la richiamiamo passiamo il file e la roba da scrivere#
     with open(filepath, 'w') as file:
--- gui.py
+++ gui.py
@@ -1,11 +1,35 @@
 import functions
 import FreeSimpleGUI as sg
 
+#from cli import new_todo
+
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter to do")
+input_box = sg.InputText(tooltip="Enter to do", key='todo')
 add_button = sg.Button("Add")
 
-window = sg.Window('My to-do App', layout=[[label], [input_box], [add_button]])
-window.read()
+window = sg.Window('My to-do App', layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+
+# associamo alle 2 variabili evento il valore della window , quindi questo conterrà i
+# valori dei button che premiamo e del testo che scriviamo
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case"Add":
+            todos = functions.get_todos()
+            ## quel #'todoo' è il rif. all inputbox che è di tipo dizionario, abbimao messo la key=todoo
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+
+        #qui intercettiamo il pulsante di chiusura per gestire la chiusura della windod, altrimenti adnrebbe in err..
+        case sg.WIN_CLOSED:
+            break
+
+        #case"Edit"
+    print("byye")
 window.close()
 
